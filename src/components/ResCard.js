0738e4449d5749d813599e37e05f4fd899355d7e
srbhau <vshirish143@gmail.com>
2025-03-23 11:01:02 +0530

    base version

--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -7,13 +7,13 @@ const ResCard = (props) => {
       <div className="res-card">
           <img
             className="item-img"
-            src={ CDN_IMAGE_URL + (resData.info.cloudinaryImageId)  }
+            src={ CDN_IMAGE_URL + (resData?.info?.cloudinaryImageId)  }
           />
         
-        <div className="res-card-text"><h3>{resData.info.name}</h3></div>
-        <div className="res-card-text"><h4>{resData.info.avgRating}Rating</h4></div>
-        <h4 className="res-card-text">{resData?.info.cuisines.join(", ")}</h4>
-        <h4 className="res-card-text">{resData?.info.deliveryTime}minutes</h4>
+        <div className="res-card-text"><h3>{resData?.info?.name}</h3></div>
+        <div className="res-card-text"><h4>{resData?.info?.avgRating}Rating</h4></div>
+        <h4 className="res-card-text">{resData?.info?.cuisines.join(", ")}</h4>
+        <h4 className="res-card-text">{resData?.info?.deliveryTime}minutes</h4>
       </div>
     );
   };
